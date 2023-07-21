9c47843de28f56a785e3dd81a5c35893496ad818
Prakash Niture <63470562+prakashniture@users.noreply.github.com>
2023-07-21 10:26:10 +0000

    datatype

---
+++ basics/dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello"); //we are using nodejs, not browser
+
+/*
+document :- 
+https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+
+https://tc39.es/ecma262/
+*/
+
+let name = "Prakash"
+let age = 18
+let isLoggedIn = true
+let state;         //undefined
+let city = null;  //null
+
+// Number
+// BigInt
+// String => ""
+// Boolean =>true/false
+// null   => standalone value
+// undefined => still value not defined
+// Symbol  => unique
+
+
+//object
+
+
+console.log(typeof "prakash"); //string
+console.log(typeof age);       //number
+console.log(typeof null);      //object
+console.log(typeof undefined); //undefined
