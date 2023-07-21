03d583b274e00a00514a3c6bcd96a94a1c8f6ae6
Prakash Niture <63470562+prakashniture@users.noreply.github.com>
2023-07-21 14:12:51 +0000

    num&math

---
+++ basics/number&math.js
@@ -0,0 +1,49 @@
+const score = 400;
+// console.log(score);
+
+const balance  = new Number(100);
+// console.log(balance);   //[Number: 100]
+
+
+// console.log(balance.toString());   //100 String
+// console.log(balance.toString().length); //3
+// console.log(balance.toFixed(2));  //100.00
+
+const otherNumber = 123.8955;
+// console.log(otherNumber.toPrecision(4));  //123.9
+
+const tenLakh = 1000000;
+// console.log(tenLakh.toLocaleString('en-IN')); //10,00,000
+
+
+// ***************** Math ***************
+
+// console.log(Math);  //Object [Math] {}
+// console.log(Math.abs(-4)); //4
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(5,4,3,2,7));
+// console.log(Math.max(1,2,3,6,3));
+// console.log(Math.random());   //0-1
+// console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10;
+const max = 20;
+
+// console.log(Math.floor(Math.random()*(max - min + 1)) + min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
