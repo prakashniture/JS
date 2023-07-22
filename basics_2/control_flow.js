c7ea49244eda628cad5fa30d600a649ab1e241e3
Prakash Niture <63470562+prakashniture@users.noreply.github.com>
2023-07-22 12:14:40 +0000

    control

--- basics_2/control_flow.js
+++ basics_2/control_flow.js
@@ -1,3 +1,14 @@
+// false  values==>> 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// true values ==>> "0", 'false', " ", [], {}, function(){}
+// (false == 0) //true
+// (false == '') //true
+// (0 == '') //true
+
+
+// == ,!=, <, >, <=, >=, ||, &&, !, ===, !===
+
+
 // if( 2 !== "2"){
 //     console.log("executed");
 // }
@@ -18,7 +29,38 @@
 // }
 
 
+const emptyObj ={}
 
+if (Object.keys(emptyObj).length === 0){
+    console.log("object is empty");
+}
+
+
+// Nullish coalescing operator (??): null undefined
+
+let val1;
+val1 = 5 ?? 10
+console.log(val1);
+
+let val2;
+val2 = null ?? 10
+console.log(val2);
+
+let val3;
+val3 = undefined ?? 20
+console.log(val3);
+
+let val4;
+val4 = null ?? 10 ?? 30
+console.log(val4);
+
+
+//Terniary operator---------
+
+//condition ? true : false
+
+const price = 100;
+price <= 80 ? console.log("less than 80") : console.log("more than 80");
 
 
 
---
+++ basics_2/switch.js
@@ -0,0 +1,19 @@
+const month = 10;
+switch (month){
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;    
+    case 4:
+        console.log("Apr");
+        break;
+    default:
+        console.log("dafault");
+    break;
+}
+
