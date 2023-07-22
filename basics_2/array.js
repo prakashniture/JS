bd44c1871a1b3006dca5845454f6ec00ec1ced33
Prakash Niture <63470562+prakashniture@users.noreply.github.com>
2023-07-22 05:56:57 +0000

    array

--- basics_2/array.js
+++ basics_2/array.js
@@ -39,14 +39,42 @@ https://developer.mozilla.org/en-US/docs/Glossary/Deep_copy
     //slice, splice-----------
 
     const myArr1 = new Array(1,2,3,4)
-    console.log("A", myArr1);  //A [ 1, 2, 3, 4 ]
+    // console.log("A", myArr1);  //A [ 1, 2, 3, 4 ]
 
     const myn1 = myArr1.slice(1, 3)
 
-    console.log(myn1);      //[ 2, 3 ]
-    console.log("B", myArr1); //B [ 1, 2, 3, 4 ]
+    // console.log(myn1);      //[ 2, 3 ]
+    // console.log("B", myArr1); //B [ 1, 2, 3, 4 ]
 
     const myn2 = myArr1.splice(1,3)
-    console.log("C", myArr1);  //C [ 1 ]
-    console.log(myn2);       //[ 2, 3, 4 ] , splice manipulate original array
-    
+    // console.log("C", myArr1);  //C [ 1 ]
+    // console.log(myn2);       //[ 2, 3, 4 ] , splice manipulate original array
+
+
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros);   //[ 'thor', 'ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+// console.log(marvel_heros[3][1]); //flash
+
+// const heros = marvel_heros.concat(dc_heros)
+//console.log(heros); //[ 'thor', 'ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+// const new_heros = [...marvel_heros, ...dc_heros];
+// console.log(new_heros);
+
+const Arr = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const anArr = Arr.flat(Infinity)  //spread all elements
+// console.log(anArr);
+
+console.log(Array.isArray("prakash"))
+console.log(Array.from("prakash"));
+
+console.log(Array.from({name:"prakash"})); //empty array
+
+let score1 = 100
+let score2 = 100
+let score3 = 100
+
+console.log(Array.of(score1, score2, score3));
