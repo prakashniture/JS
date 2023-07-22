8afcec6bae5e2d03148a1d5c9a50fb0a9e7215ea
Prakash Niture <63470562+prakashniture@users.noreply.github.com>
2023-07-22 08:02:21 +0000

    function

--- basics_2/function.js
+++ basics_2/function.js
@@ -30,13 +30,45 @@
 
 
 
-function user(userName){   // or function user(userName = "abc")
-    if(!userName){
-        console.log("please enter username");
-        return;
-    }
-    return `${userName} just logged in`;
-}
+// function user(userName){   // or function user(userName = "abc")
+//     if(!userName){
+//         console.log("please enter username");
+//         return;
+//     }
+//     return `${userName} just logged in`;
+//}
 // console.log(user("abc"));
-console.log(user());    //undefined
+// console.log(user());    //undefined
+
+
+
+function calC(...num1) {
+    return num1;
+}
+// console.log(calC(200, 400, 500));  
+
+
+
+const user = {
+    userName: "abc",
+    price: 200
+}
+function handleObject(anyObject){
+    // console.log(`username is ${anyObject.userName} and price is ${anyObject.price}`);
+}
+// handleObject(user);
+
+handleObject({
+    userName:"zxy",
+    price: 400
+})
+
+const newArray = [200, 400, 100, 500]
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+// console.log(newArray);
+// console.log(returnSecondValue([200, 499, 599, 654]));
+
+
 
