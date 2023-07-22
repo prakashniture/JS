91922ff83f96f2cd8ed4d560bf98210f125a075a
Prakash Niture <63470562+prakashniture@users.noreply.github.com>
2023-07-22 11:44:49 +0000

    control

---
+++ basics_2/control_flow.js
@@ -0,0 +1,24 @@
+// if( 2 !== "2"){
+//     console.log("executed");
+// }
+
+
+// const score = 200;
+// if(score > 100){
+//     const power = "fly";
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);  //Error
+
+
+
+// if(condition){
+// } else if(condition){
+// }
+
+
+
+
+
+
