c77446c4e0c97967e8ac66e45460996669b35a16
Prakash Niture <63470562+prakashniture@users.noreply.github.com>
2023-07-23 03:39:40 +0000

    loop

---
+++ basics_2/loop.js
@@ -0,0 +1,31 @@
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if(element ==5){
+//         console.log("5 is best number");
+//     }
+//     console.log(element);
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         // console.log('Inner loop value ${j} and inner loop ${i}');        
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }   
+// }
+
+
+let myArray = ["flash", "batman", "superman"]
+console.log(myArray.length);
+for
+
+
+
+
